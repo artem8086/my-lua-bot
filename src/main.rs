8039b3fbc2a14be4da9812f8ac8d35425333e8eb
artyom.svyatoha <6Jyc2Pp4WY>
2020-09-07 14:41:41 +0300

    get port from variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
+use std::env;
+
 mod lua;
 
 #[derive(Deserialize)]
@@ -14,11 +16,17 @@ async fn index(lua: web::Json<LuaCode>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::vars()
+        .find(|(name, _)| name.eq("PORT"))
+        .unwrap_or((String::from("PORT"), String::from("8088"))).1;
+
+    println!("Server run at port: {}", port);
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
     })
-        .bind("127.0.0.1:8088")?
+        .bind(format!("127.0.0.1:{}", port))?
         .run()
         .await
 }
